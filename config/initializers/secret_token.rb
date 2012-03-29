9da5129a868b553d930312180dd6712352c36535
BrandonMathis <Brandon@KeysetTS.com>
2012-03-28 22:51:02 -0400

    Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badiframe::Application.config.secret_token = '3c4aafc27fa0fce45f41a735a0ac7ba3652eb8aa386e4605cc48e48a2a03703075109716789830b584834c2223bd20c0769b5ec6327f2b87420b611ade725bc3'
